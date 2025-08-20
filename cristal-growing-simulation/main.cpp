893088a230704c4f4efc3b50b9dd2d1fb36d6e52
Carsten Daub <carsten.dau@gmail.com>
2025-08-20 14:38:45 +0200

    Initial Commit

---
+++ cristal-growing-simulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cristal-growing-simulation
+//
+//  Created by Carsten Daub on 2025-08-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
