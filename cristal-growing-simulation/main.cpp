5aca39b7afae71e69f8bfa5b18fcf8c27aa99d02
Carsten Daub <carsten.dau@gmail.com>
2025-08-21 10:01:25 +0200

    remove Hello World text

--- cristal-growing-simulation/main.cpp
+++ cristal-growing-simulation/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
+#include <random>
+
+// Function to generate a random number
+int randomNumber() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> distrib(0, 5);
+    return distrib(gen);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    
+    int myRand = randomNumber();
+    std::cout << "Random Number: " << myRand << "\n";
+
     return 0;
 }
